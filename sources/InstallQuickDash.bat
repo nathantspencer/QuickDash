91b7707858c0c6a314153fa6fabe0b5ba017cdec
Nathan Spencer <nathantspencer@gmail.com>
2025-01-04 16:48:22 -0500

    Working installer
    Symlink creation removed due to need for admin privilege; EXE installed directly into Startup directory instead

--- sources/InstallQuickDash.bat
+++ sources/InstallQuickDash.bat
@@ -4,9 +4,8 @@
 
 rem This is the QuickDash installation script, which:
 rem     (1) Kills any existing QuickDash process
-rem     (2) Installs QuickDash executable in AppData
-rem     (3) Places a symbolic link in the Startup directory
-rem     (4) Runs the executable for immediate use
+rem     (2) Installs the QuickDash executable in the Startup directory
+rem     (3) Runs the executable for immediate use
 
 rem Ensure that QuickDash.exe exists in the current directory; a prerequisite for installation.
 
@@ -20,10 +19,10 @@ rem Kill any existing QuickDash processes prior to installation.
 echo Stopping execution of previous QuickDash installations...
 taskkill /im QuickDash.exe /f /fi "STATUS eq RUNNING" > nul
 
-rem Copy the QuickDash executable to a QuickDash folder in AppData.
+rem Copy the QuickDash executable to the Startup folder for automatic launching on future startups.
 
 echo Installing QuickDash executable...
-xcopy "QuickDash.exe" "%APPDATA%\QuickDash\" /y > nul
+xcopy "QuickDash.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /y > nul
 if errorlevel 4 (
     echo [91mINSTALLATION ERROR:[0m Initialization error during copy; ensure disk/memory space is available.
     goto end
@@ -37,10 +36,10 @@ if not errorlevel 0 (
     goto end
 )
 
-rem TODO: Create the symbolic link via mklink
+rem Launch QuickDash, allowing it to be used immediately without restart.
 
 echo Launching QuickDash executable for immediate use...
-start %APPDATA%\QuickDash\QuickDash.exe
+start QuickDash.exe
 
 rem Herein lies the QuickDash logo art. Thanks to the use escape characters and colors, it looks a little unsavory here.
 
@@ -50,7 +49,7 @@ echo        [91m______[0m    _____            _           _      ______       
 echo       [91m^|______^|[0m  / ___ \          '_'         ^| ^|    ^|  ___ \                   ^| ^|      
 echo       [93m_______[0m  ^| /   \ ^|  _   _   _    ____  ^| ^| __ ^| ^|   \ ^|   ___ _   _____  ^| ^|___   
 echo      [93m^|_______^|[0m ^| ^|  _^| ^| ^| ^| ^| ^| ^| ^|  /  __^| ^| ^|/ / ^| ^|   ^| ^|  / __` ^| /  ___^| ^| '__ \  
-echo   [92m^___________[0m  ^| \__\ `^| ^| \_^| ^| ^| ^| ^|  '__  ^|   ^<  ^| ^|___/ ^| ^| '__^| ^| \___  \ ^| ^|  ` ^| 
+echo   [92m^___________[0m  ^| \__\ `' ^| \_^| ^| ^| ^| ^|  '__  ^|   ^<  ^| ^|___/ ^| ^| '__^| ^| \___  \ ^| ^|  ` ^| 
 echo  [92m^|___________^|[0m  \___,\_\  \__,_^| ^|_^|  \____^| ^|_^|\_\ ^|______/   \___,_^| ^|____,/ ^|_^|  ^|_^| 
 echo:
 echo                           [96mH A S   B E E N   I N S T A L L E D   S U C C E S S F U L L Y[0m 
@@ -61,7 +60,7 @@ echo:
 echo:
 echo Need support? Visit github.com/nathantspencer/QuickDash.
 echo: 
-echo This window will close after 20 seconds or when any key is pressed.
+echo This window will close after 15 seconds or when any key is pressed.
 echo:
 
-timeout /t 20 > nul
+timeout /t 15 > nul
